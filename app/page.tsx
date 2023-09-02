53d247f5597442e0a3571f1fda8b1e6fd1701b53
Ronel <ronel.kpossou@epitech.eu>
2023-09-02 21:10:16 +0100

    Add constants & review layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-24">
+    <main className="p-24">
       <ModeToggle />
       <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mt-6">
         Taxing Laughter: The Joke Tax Chronicles
---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = "app";
+export const APP_DESCRIPTION = "app description";
+export const APP_KEYWORDS = "app keywords";
+export const APP_AUTHOR = [];
+export const APP_URL = "app url";
